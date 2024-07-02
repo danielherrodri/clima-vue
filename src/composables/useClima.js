83a2ecc736d7465e8f83a697300cc4ab9ced4c00
drodriguez <drodriguez@izzi.mx>
2024-07-02 10:53:31 -0600

    Vídeo 173 - Extrayendo valores de la consulta previa

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,8 +1,15 @@
+import axios from "axios";
 export default function useClima() {
-  const obtenerClima = ({ ciudad, pais }) => {
+  const obtenerClima = async ({ ciudad, pais }) => {
     const key = import.meta.env.VITE_API_KEY;
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
-    console.log(url)
+    try {
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+      const { data } = await axios(url);
+      const { lat, lon } = data[0];
+      console.log(lat, lon)
+    } catch (error) {
+      console.log(error);
+    }
   };
   return {
     obtenerClima,
