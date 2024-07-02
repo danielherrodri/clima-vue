1bec1fea2851422b4bdb6f438165d6764c6a4770
drodriguez <drodriguez@izzi.mx>
2024-07-02 10:47:15 -0600

    Vídeo 172 - Primer llamado hacia la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,6 +1,8 @@
 export default function useClima() {
   const obtenerClima = ({ ciudad, pais }) => {
     const key = import.meta.env.VITE_API_KEY;
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+    console.log(url)
   };
   return {
     obtenerClima,
