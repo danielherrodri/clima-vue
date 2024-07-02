7390f290767eb89d3df1bb390b28450de6cb6a49
drodriguez <drodriguez@izzi.mx>
2024-07-02 10:59:41 -0600

    Vídeo 174 - Consultando el clima

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -6,7 +6,10 @@ export default function useClima() {
       const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
       const { data } = await axios(url);
       const { lat, lon } = data[0];
-      console.log(lat, lon)
+
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+      const { data: resultado } = await axios(urlClima);
+      console.log(resultado);
     } catch (error) {
       console.log(error);
     }
